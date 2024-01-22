23e641388b9ea1c8961ff46ef1c9848468a2ed7c
Mehmet Karaca <mekaraca18@gmail.com>
2024-01-23 01:29:35 +0300

    Initial commit

---
+++ app/src/main/java/com/karacamehmet/mediaplayer/MainActivity.kt
@@ -0,0 +1,180 @@
+package com.karacamehmet.mediaplayer
+
+import android.media.MediaMetadataRetriever
+import android.media.MediaPlayer
+import android.net.Uri
+import android.opengl.Visibility
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
+import android.widget.SeekBar
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.os.postDelayed
+import com.karacamehmet.mediaplayer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var audioMediaPlayer: MediaPlayer? = null
+    private var videoMediaPlayer: MediaPlayer? = null
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            uri?.let {
+                when (getMediaType(it)) {
+                    MediaType.AUDIO -> playAudioFile(it)
+                    MediaType.VIDEO -> playVideoFile(it)
+                }
+            }
+        }
+    private var isUserSeeking = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonAudio.setOnClickListener {
+            getContent.launch("audio/*")
+        }
+        binding.buttonVideo.setOnClickListener {
+            getContent.launch("video/*")
+        }
+        binding.imageButtonPauseResume.setOnClickListener {
+            if (audioMediaPlayer != null) {
+                if (audioMediaPlayer?.isPlaying!!) {
+                    audioMediaPlayer?.pause()
+                    binding.imageButtonPauseResume.setImageDrawable(getDrawable(R.drawable.baseline_play_circle_24))
+                } else {
+                    audioMediaPlayer?.start()
+                    binding.imageButtonPauseResume.setImageDrawable(getDrawable(R.drawable.baseline_pause_circle_24))
+                }
+
+            }
+        }
+        binding.imageButtonStop.setOnClickListener {
+            if (audioMediaPlayer != null) {
+                setVisibilityToNeutral()
+                audioMediaPlayer?.reset()
+            }
+        }
+        binding.audioSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    audioMediaPlayer?.seekTo(progress)
+                }
+                if (progress==seekBar?.max){
+                    binding.imageButtonPauseResume.setImageDrawable(getDrawable(R.drawable.baseline_play_circle_24))
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                isUserSeeking = true
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                isUserSeeking = false
+            }
+
+        })
+    }
+
+    private fun playAudioFile(uri: Uri) {
+        setVisibilityToAudio()
+        audioMediaPlayer?.release()
+        videoMediaPlayer?.release()
+        audioMediaPlayer = MediaPlayer().apply {
+            setDataSource(this@MainActivity, uri)
+            prepare()
+            start()
+            binding.audioSeekBar.max = duration
+            binding.textViewAudioTotalTime.text = formatTime(duration)
+            Handler(Looper.getMainLooper()).postDelayed({
+                updateSeekBar()
+            }, 1000)
+        }
+    }
+
+    private fun playVideoFile(uri: Uri) {
+        setVisibilityToVideo()
+        videoMediaPlayer?.release()
+        audioMediaPlayer?.reset()
+        videoMediaPlayer = MediaPlayer().apply {
+            setDataSource(this@MainActivity, uri)
+            binding.videoView.setMediaController(android.widget.MediaController(this@MainActivity))
+            binding.videoView.setVideoURI(uri)
+            binding.videoView.requestFocus()
+            start()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        audioMediaPlayer?.release()
+        videoMediaPlayer?.release()
+    }
+
+
+    private fun getMediaType(uri: Uri): MediaType {
+        val retriever = MediaMetadataRetriever()
+        retriever.setDataSource(this, uri)
+
+        val hasVideo = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_HAS_VIDEO)
+        val hasAudio = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_HAS_AUDIO)
+        return when {
+            hasVideo != null && hasVideo == "yes" -> MediaType.VIDEO
+            hasAudio != null && hasAudio == "yes" -> MediaType.AUDIO
+            else -> throw Exception()
+        }
+    }
+
+    enum class MediaType {
+        AUDIO, VIDEO
+    }
+
+    private fun updateSeekBar() {
+        if (!isUserSeeking) {
+            val currentPosition = audioMediaPlayer?.currentPosition ?: 0
+            binding.audioSeekBar.progress = currentPosition
+            binding.textViewAudioCurrentTime.text = formatTime(currentPosition)
+        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            updateSeekBar()
+        }, 1000)
+    }
+
+    private fun setVisibilityToAudio() {
+        binding.imageButtonPauseResume.setImageDrawable(getDrawable(R.drawable.baseline_pause_circle_24))
+        binding.imageButtonPauseResume.visibility = View.VISIBLE
+        binding.audioSeekBar.visibility = View.VISIBLE
+        binding.textViewAudioCurrentTime.visibility = View.VISIBLE
+        binding.textViewAudioTotalTime.visibility = View.VISIBLE
+        binding.imageButtonStop.visibility = View.VISIBLE
+        binding.videoView.visibility = View.INVISIBLE
+    }
+
+    private fun setVisibilityToVideo() {
+        binding.imageButtonPauseResume.visibility = View.INVISIBLE
+        binding.audioSeekBar.visibility = View.INVISIBLE
+        binding.textViewAudioCurrentTime.visibility = View.INVISIBLE
+        binding.textViewAudioTotalTime.visibility = View.INVISIBLE
+        binding.imageButtonStop.visibility = View.INVISIBLE
+        binding.videoView.visibility = View.VISIBLE
+    }
+
+    private fun setVisibilityToNeutral() {
+        binding.imageButtonPauseResume.visibility = View.INVISIBLE
+        binding.audioSeekBar.visibility = View.INVISIBLE
+        binding.textViewAudioCurrentTime.visibility = View.INVISIBLE
+        binding.textViewAudioTotalTime.visibility = View.INVISIBLE
+        binding.imageButtonStop.visibility = View.INVISIBLE
+        binding.videoView.visibility = View.INVISIBLE
+    }
+    private fun formatTime(timeInMilliseconds: Int): String {
+        val seconds = timeInMilliseconds / 1000
+        val minutes = seconds / 60
+        val remainingSeconds = seconds % 60
+        return String.format("%02d:%02d", minutes, remainingSeconds)
+    }
+
+}
